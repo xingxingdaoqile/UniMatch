421f3c5982566c91b1a213488d6655886484704f
xingxing <1765658894@qq.com>
2024-09-27 16:20:01 +0800

    Update train.sh

--- scripts/train.sh
+++ scripts/train.sh
@@ -12,10 +12,14 @@ method='unimatch'
 exp='r101'
 split='1_2'
 
-config=configs/${dataset}.yaml
-labeled_id_path=splits/$dataset/$split/labeled.txt
-unlabeled_id_path=splits/$dataset/$split/unlabeled.txt
-save_path=exp/$dataset/$method/$exp/$split
+#config=configs/${dataset}.yaml
+config=/kaggle/working/UniMatch/configs/${dataset}.yaml
+#labeled_id_path=splits/$dataset/$split/labeled.txt
+labeled_id_path=/kaggle/working/UniMatch/splits/$dataset/$split/labeled.txt
+#unlabeled_id_path=splits/$dataset/$split/unlabeled.txt
+unlabeled_id_path=/kaggle/working/UniMatch/splits/$dataset/$split/unlabeled.txt
+#save_path=exp/$dataset/$method/$exp/$split
+save_path=/kaggle/working/UniMatch/exp/$dataset/$method/$exp/$split
 
 mkdir -p $save_path
 
@@ -30,7 +34,7 @@ python -m torch.distributed.launch \
     --nproc_per_node=$1 \
     --master_addr=localhost \
     --master_port=$2 \
-    /kaggle/working/UniMatch/unimatch.py \
+    /kaggle/working/UniMatch/$method.py \
     --config=$config --labeled-id-path $labeled_id_path --unlabeled-id-path $unlabeled_id_path \
     --save-path $save_path --port $2 2>&1 | tee $save_path/$now.log
 

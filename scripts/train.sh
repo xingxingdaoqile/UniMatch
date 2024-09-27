609130a7b4893aebfedce45e53c4f957f30d2799
xingxing <1765658894@qq.com>
2024-09-27 16:05:47 +0800

    1
    1

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-now=$(date +"%Y%m%d_%H%M%S")#这行代码使用 date 命令获取当前日期和时间，并将其格式化为 YYYYMMDD_HHMMSS 格式。该变量 now 之后会被用来命名日志文件，确保每次运行时生成的日志文件不会覆盖掉之前的日志。
+#now=$(date +"%Y%m%d_%H%M%S")#这行代码使用 date 命令获取当前日期和时间，并将其格式化为 YYYYMMDD_HHMMSS 格式。该变量 now 之后会被用来命名日志文件，确保每次运行时生成的日志文件不会覆盖掉之前的日志。
+now=$(date +"%Y%m%d_%H%M%S")
 
 # modify these augments if you want to try other datasets, splits or methods
 # dataset: ['pascal', 'cityscapes', 'coco']
@@ -18,13 +19,21 @@ save_path=exp/$dataset/$method/$exp/$split
 
 mkdir -p $save_path
 
-python -m torch.distributed.launch \
+'''python -m torch.distributed.launch \
     --nproc_per_node=$1 \
     --master_addr=localhost \
     --master_port=$2 \
     $method.py \
     --config=$config --labeled-id-path $labeled_id_path --unlabeled-id-path $unlabeled_id_path \
+    --save-path $save_path --port $2 2>&1 | tee $save_path/$now.log'''
+python -m torch.distributed.launch \
+    --nproc_per_node=$1 \
+    --master_addr=localhost \
+    --master_port=$2 \
+    /kaggle/working/UniMatch/unimatch.py \
+    --config=$config --labeled-id-path $labeled_id_path --unlabeled-id-path $unlabeled_id_path \
     --save-path $save_path --port $2 2>&1 | tee $save_path/$now.log
+
  #--nproc_per_node=$1：指定每个节点的进程数，由脚本的第一个参数 $1 决定（例如 GPU 数量）。
  #--master_addr=localhost：设置主节点的地址为 localhost（即本地机器）。
  #--master_port=$2：设置主节点通信的端口，由脚本的第二个参数 $2 决定。

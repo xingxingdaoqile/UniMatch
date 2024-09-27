e4d52a704c7f07d4fa67fa5fdb3730e09dad3b7d
xingxing <1765658894@qq.com>
2024-09-27 21:49:26 +0800

    Update semi.py

--- dataset/semi.py
+++ dataset/semi.py
@@ -31,14 +31,11 @@ class SemiDataset(Dataset):
 
     def __getitem__(self, item):
         id = self.ids[item]
-        #img = Image.open(os.path.join(self.root, id.split(' ')[0])).convert('RGB')
-        img = Image.open(
-            os.path.join('/kaggle/input/cityscapes/leftImg8bit_trainvaltest', id.split(' ')[0])).convert(
-            'RGB')
+        img = Image.open(os.path.join(self.root, id.split(' ')[0])).convert('RGB')
+        #img = Image.open(os.path.join('/kaggle/input/cityscapes/leftImg8bit_trainvaltest', id.split(' ')[0])).convert('RGB')
 
-        #mask = Image.fromarray(np.array(Image.open(os.path.join(self.root, id.split(' ')[1]))))
-        mask = Image.fromarray(
-            np.array(Image.open(os.path.join('/kaggle/input/cityscapes/gtFine_trainvaltest', id.split(' ')[1]))))
+        mask = Image.fromarray(np.array(Image.open(os.path.join(self.root, id.split(' ')[1]))))
+        #mask = Image.fromarray(np.array(Image.open(os.path.join('/kaggle/input/cityscapes/gtFine_trainvaltest', id.split(' ')[1]))))
 
         if self.mode == 'val':
             img, mask = normalize(img, mask)

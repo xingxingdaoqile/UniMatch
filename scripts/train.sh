7c315ba492ed74905f9e313f355e70acb8ddb62a
xingxing <1765658894@qq.com>
2024-09-27 15:27:32 +0800

    Initial commit

---
+++ scripts/train.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+now=$(date +"%Y%m%d_%H%M%S")#这行代码使用 date 命令获取当前日期和时间，并将其格式化为 YYYYMMDD_HHMMSS 格式。该变量 now 之后会被用来命名日志文件，确保每次运行时生成的日志文件不会覆盖掉之前的日志。
+
+# modify these augments if you want to try other datasets, splits or methods
+# dataset: ['pascal', 'cityscapes', 'coco']
+# method: ['unimatch', 'fixmatch', 'supervised']
+# exp: just for specifying the 'save_path'
+# split: ['92', '1_16', 'u2pl_1_16', ...]. Please check directory './splits/$dataset' for concrete splits
+dataset='cityscapes'
+method='unimatch'
+exp='r101'
+split='1_2'
+
+config=configs/${dataset}.yaml
+labeled_id_path=splits/$dataset/$split/labeled.txt
+unlabeled_id_path=splits/$dataset/$split/unlabeled.txt
+save_path=exp/$dataset/$method/$exp/$split
+
+mkdir -p $save_path
+
+python -m torch.distributed.launch \
+    --nproc_per_node=$1 \
+    --master_addr=localhost \
+    --master_port=$2 \
+    $method.py \
+    --config=$config --labeled-id-path $labeled_id_path --unlabeled-id-path $unlabeled_id_path \
+    --save-path $save_path --port $2 2>&1 | tee $save_path/$now.log
+ #--nproc_per_node=$1：指定每个节点的进程数，由脚本的第一个参数 $1 决定（例如 GPU 数量）。
+ #--master_addr=localhost：设置主节点的地址为 localhost（即本地机器）。
+ #--master_port=$2：设置主节点通信的端口，由脚本的第二个参数 $2 决定。
+
+ #2>&1：将标准错误输出重定向到标准输出，使得所有输出（包括错误信息）都可以记录。
+ #tee $save_path/$now.log：使用 tee 命令将输出同时保存到终端和日志文件 $save_path/$now.log 中。
---
+++ unimatch.py
@@ -0,0 +1,252 @@
+import argparse
+import logging
+import os
+import pprint
+
+import torch
+from torch import nn
+import torch.backends.cudnn as cudnn
+from torch.optim import SGD
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+import yaml
+
+from dataset.semi import SemiDataset
+from model.semseg.deeplabv3plus import DeepLabV3Plus
+from supervised import evaluate
+from util.classes import CLASSES
+from util.ohem import ProbOhemCrossEntropy2d
+from util.utils import count_params, init_log, AverageMeter
+from util.dist_helper import setup_distributed
+
+
+parser = argparse.ArgumentParser(description='Revisiting Weak-to-Strong Consistency in Semi-Supervised Semantic Segmentation')
+parser.add_argument('--config', type=str, required=True)
+parser.add_argument('--labeled-id-path', type=str, required=True)
+parser.add_argument('--unlabeled-id-path', type=str, required=True)
+parser.add_argument('--save-path', type=str, required=True)
+parser.add_argument('--local_rank', default=0, type=int)
+parser.add_argument('--port', default=None, type=int)
+
+
+def main():
+    args = parser.parse_args()
+
+    cfg = yaml.load(open(args.config, "r"), Loader=yaml.Loader)#加载配置文件的内容，并转换为字典格式供后续使用
+
+    logger = init_log('global', logging.INFO)#调用 init_log 初始化日志，设置全局日志等级为 INFO，输出训练和评估过程中的重要信息
+    logger.propagate = 0 #关闭日志的向上传播，避免重复打印日志信息
+
+    rank, world_size = setup_distributed(port=args.port)#调用 setup_distributed 函数，配置分布式训练环境，返回当前进程的 rank 和总进程数 world_size。分布式训练可以在多个 GPU 上同时进行，从而加速训练过程。
+
+    if rank == 0:#表示这是主进程（主节点），仅主进程负责日志输出和模型保存
+        all_args = {**cfg, **vars(args), 'ngpus': world_size}#all_args 合并配置文件和命令行参数，并输出日志记录所有配置参数，以便之后查看和复现实验。
+        logger.info('{}\n'.format(pprint.pformat(all_args)))
+        
+        writer = SummaryWriter(args.save_path)#用于将训练过程的损失、评估指标写入 TensorBoard，便于可视化
+        
+        os.makedirs(args.save_path, exist_ok=True)
+    
+    cudnn.enabled = True#启用 cudnn 加速，cudnn.benchmark = True 可以通过动态调整选择最合适的卷积算法来加速训练。
+    cudnn.benchmark = True
+
+    model = DeepLabV3Plus(cfg)#根据配置文件初始化 DeepLabV3Plus 模型，它是一个常用的语义分割网络结构
+    optimizer = SGD([{'params': model.backbone.parameters(), 'lr': cfg['lr']},
+                    {'params': [param for name, param in model.named_parameters() if 'backbone' not in name],
+                    'lr': cfg['lr'] * cfg['lr_multi']}], lr=cfg['lr'], momentum=0.9, weight_decay=1e-4)
+    #使用 SGD 优化器，分别为模型的骨干网络 backbone 和其他部分设置不同的学习率。这样可以对不同层的参数施加不同的学习率进行优化。
+    if rank == 0:#统计并输出模型的参数总量（以百万为单位），只有主进程需要记录这些信息。
+        logger.info('Total params: {:.1f}M\n'.format(count_params(model)))
+    #获取本地 GPU 编号，并将模型放入 GPU 中 (model.cuda())。
+    #使用 SyncBatchNorm 处理同步批归一化，使得在多个 GPU 上训练时，批归一化统计值保持一致。
+    #使用 DistributedDataParallel 包装模型，支持分布式并行训练。
+    local_rank = int(os.environ["LOCAL_RANK"])
+    model = torch.nn.SyncBatchNorm.convert_sync_batchnorm(model)
+    model.cuda()
+
+    model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[local_rank], broadcast_buffers=False,
+                                                      output_device=local_rank, find_unused_parameters=False)
+
+    if cfg['criterion']['name'] == 'CELoss':
+        criterion_l = nn.CrossEntropyLoss(**cfg['criterion']['kwargs']).cuda(local_rank)
+    elif cfg['criterion']['name'] == 'OHEM':
+        criterion_l = ProbOhemCrossEntropy2d(**cfg['criterion']['kwargs']).cuda(local_rank)
+    else:
+        raise NotImplementedError('%s criterion is not implemented' % cfg['criterion']['name'])
+
+    criterion_u = nn.CrossEntropyLoss(reduction='none').cuda(local_rank)#是无监督数据的损失函数，计算无标注数据的损失时，使用交叉熵但不进行归约（保留原始的逐像素损失）
+
+    trainset_u = SemiDataset(cfg['dataset'], cfg['data_root'], 'train_u',
+                             cfg['crop_size'], args.unlabeled_id_path)
+    trainset_l = SemiDataset(cfg['dataset'], cfg['data_root'], 'train_l',
+                             cfg['crop_size'], args.labeled_id_path, nsample=len(trainset_u.ids))
+    valset = SemiDataset(cfg['dataset'], cfg['data_root'], 'val')#nsample 确保有标注数据和无标注数据数量一致，以便在半监督学习中进行对比
+
+    trainsampler_l = torch.utils.data.distributed.DistributedSampler(trainset_l)#创建数据采样器 DistributedSampler，用于分布式训练环境，确保不同进程能够加载不同的数据切片。
+    trainloader_l = DataLoader(trainset_l, batch_size=cfg['batch_size'],
+                               pin_memory=True, num_workers=1, drop_last=True, sampler=trainsampler_l)#DataLoader 用于将数据集打包为批次，并分发到 GPU 进行训练和验证
+    trainsampler_u = torch.utils.data.distributed.DistributedSampler(trainset_u)
+    trainloader_u = DataLoader(trainset_u, batch_size=cfg['batch_size'],
+                               pin_memory=True, num_workers=1, drop_last=True, sampler=trainsampler_u)
+    valsampler = torch.utils.data.distributed.DistributedSampler(valset)
+    valloader = DataLoader(valset, batch_size=1, pin_memory=True, num_workers=1,
+                           drop_last=False, sampler=valsampler)
+
+    total_iters = len(trainloader_u) * cfg['epochs']
+    previous_best = 0.0
+    epoch = -1
+    
+    if os.path.exists(os.path.join(args.save_path, 'latest.pth')):
+        checkpoint = torch.load(os.path.join(args.save_path, 'latest.pth'))
+        model.load_state_dict(checkpoint['model'])
+        optimizer.load_state_dict(checkpoint['optimizer'])
+        epoch = checkpoint['epoch']
+        previous_best = checkpoint['previous_best']
+        
+        if rank == 0:
+            logger.info('************ Load from checkpoint at epoch %i\n' % epoch)
+    
+    for epoch in range(epoch + 1, cfg['epochs']):
+        if rank == 0:
+            logger.info('===========> Epoch: {:}, LR: {:.5f}, Previous best: {:.2f}'.format(
+                epoch, optimizer.param_groups[0]['lr'], previous_best))
+
+        total_loss = AverageMeter()
+        total_loss_x = AverageMeter()
+        total_loss_s = AverageMeter()
+        total_loss_w_fp = AverageMeter()
+        total_mask_ratio = AverageMeter()
+
+        trainloader_l.sampler.set_epoch(epoch)
+        trainloader_u.sampler.set_epoch(epoch)
+
+        loader = zip(trainloader_l, trainloader_u, trainloader_u)
+
+        for i, ((img_x, mask_x),
+                (img_u_w, img_u_s1, img_u_s2, ignore_mask, cutmix_box1, cutmix_box2),
+                (img_u_w_mix, img_u_s1_mix, img_u_s2_mix, ignore_mask_mix, _, _)) in enumerate(loader):
+            
+            img_x, mask_x = img_x.cuda(), mask_x.cuda()
+            img_u_w = img_u_w.cuda()
+            img_u_s1, img_u_s2, ignore_mask = img_u_s1.cuda(), img_u_s2.cuda(), ignore_mask.cuda()
+            cutmix_box1, cutmix_box2 = cutmix_box1.cuda(), cutmix_box2.cuda()
+            img_u_w_mix = img_u_w_mix.cuda()
+            img_u_s1_mix, img_u_s2_mix = img_u_s1_mix.cuda(), img_u_s2_mix.cuda()
+            ignore_mask_mix = ignore_mask_mix.cuda()
+
+            with torch.no_grad():
+                model.eval()
+
+                pred_u_w_mix = model(img_u_w_mix).detach()
+                conf_u_w_mix = pred_u_w_mix.softmax(dim=1).max(dim=1)[0]
+                mask_u_w_mix = pred_u_w_mix.argmax(dim=1)
+
+            img_u_s1[cutmix_box1.unsqueeze(1).expand(img_u_s1.shape) == 1] = \
+                img_u_s1_mix[cutmix_box1.unsqueeze(1).expand(img_u_s1.shape) == 1]
+            img_u_s2[cutmix_box2.unsqueeze(1).expand(img_u_s2.shape) == 1] = \
+                img_u_s2_mix[cutmix_box2.unsqueeze(1).expand(img_u_s2.shape) == 1]
+
+            model.train()
+
+            num_lb, num_ulb = img_x.shape[0], img_u_w.shape[0]
+
+            preds, preds_fp = model(torch.cat((img_x, img_u_w)), True)
+            pred_x, pred_u_w = preds.split([num_lb, num_ulb])
+            pred_u_w_fp = preds_fp[num_lb:]
+
+            pred_u_s1, pred_u_s2 = model(torch.cat((img_u_s1, img_u_s2))).chunk(2)
+
+            pred_u_w = pred_u_w.detach()
+            conf_u_w = pred_u_w.softmax(dim=1).max(dim=1)[0]
+            mask_u_w = pred_u_w.argmax(dim=1)
+
+            mask_u_w_cutmixed1, conf_u_w_cutmixed1, ignore_mask_cutmixed1 = \
+                mask_u_w.clone(), conf_u_w.clone(), ignore_mask.clone()
+            mask_u_w_cutmixed2, conf_u_w_cutmixed2, ignore_mask_cutmixed2 = \
+                mask_u_w.clone(), conf_u_w.clone(), ignore_mask.clone()
+
+            mask_u_w_cutmixed1[cutmix_box1 == 1] = mask_u_w_mix[cutmix_box1 == 1]
+            conf_u_w_cutmixed1[cutmix_box1 == 1] = conf_u_w_mix[cutmix_box1 == 1]
+            ignore_mask_cutmixed1[cutmix_box1 == 1] = ignore_mask_mix[cutmix_box1 == 1]
+
+            mask_u_w_cutmixed2[cutmix_box2 == 1] = mask_u_w_mix[cutmix_box2 == 1]
+            conf_u_w_cutmixed2[cutmix_box2 == 1] = conf_u_w_mix[cutmix_box2 == 1]
+            ignore_mask_cutmixed2[cutmix_box2 == 1] = ignore_mask_mix[cutmix_box2 == 1]
+
+            loss_x = criterion_l(pred_x, mask_x)
+
+            loss_u_s1 = criterion_u(pred_u_s1, mask_u_w_cutmixed1)
+            loss_u_s1 = loss_u_s1 * ((conf_u_w_cutmixed1 >= cfg['conf_thresh']) & (ignore_mask_cutmixed1 != 255))
+            loss_u_s1 = loss_u_s1.sum() / (ignore_mask_cutmixed1 != 255).sum().item()
+
+            loss_u_s2 = criterion_u(pred_u_s2, mask_u_w_cutmixed2)
+            loss_u_s2 = loss_u_s2 * ((conf_u_w_cutmixed2 >= cfg['conf_thresh']) & (ignore_mask_cutmixed2 != 255))
+            loss_u_s2 = loss_u_s2.sum() / (ignore_mask_cutmixed2 != 255).sum().item()
+
+            loss_u_w_fp = criterion_u(pred_u_w_fp, mask_u_w)
+            loss_u_w_fp = loss_u_w_fp * ((conf_u_w >= cfg['conf_thresh']) & (ignore_mask != 255))
+            loss_u_w_fp = loss_u_w_fp.sum() / (ignore_mask != 255).sum().item()
+
+            loss = (loss_x + loss_u_s1 * 0.25 + loss_u_s2 * 0.25 + loss_u_w_fp * 0.5) / 2.0
+
+            torch.distributed.barrier()
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            total_loss.update(loss.item())
+            total_loss_x.update(loss_x.item())
+            total_loss_s.update((loss_u_s1.item() + loss_u_s2.item()) / 2.0)
+            total_loss_w_fp.update(loss_u_w_fp.item())
+            
+            mask_ratio = ((conf_u_w >= cfg['conf_thresh']) & (ignore_mask != 255)).sum().item() / \
+                (ignore_mask != 255).sum()
+            total_mask_ratio.update(mask_ratio.item())
+
+            iters = epoch * len(trainloader_u) + i
+            lr = cfg['lr'] * (1 - iters / total_iters) ** 0.9
+            optimizer.param_groups[0]["lr"] = lr
+            optimizer.param_groups[1]["lr"] = lr * cfg['lr_multi']
+            
+            if rank == 0:
+                writer.add_scalar('train/loss_all', loss.item(), iters)
+                writer.add_scalar('train/loss_x', loss_x.item(), iters)
+                writer.add_scalar('train/loss_s', (loss_u_s1.item() + loss_u_s2.item()) / 2.0, iters)
+                writer.add_scalar('train/loss_w_fp', loss_u_w_fp.item(), iters)
+                writer.add_scalar('train/mask_ratio', mask_ratio, iters)
+            
+            if (i % (len(trainloader_u) // 8) == 0) and (rank == 0):
+                logger.info('Iters: {:}, Total loss: {:.3f}, Loss x: {:.3f}, Loss s: {:.3f}, Loss w_fp: {:.3f}, Mask ratio: '
+                            '{:.3f}'.format(i, total_loss.avg, total_loss_x.avg, total_loss_s.avg,
+                                            total_loss_w_fp.avg, total_mask_ratio.avg))
+
+        eval_mode = 'sliding_window' if cfg['dataset'] == 'cityscapes' else 'original'
+        mIoU, iou_class = evaluate(model, valloader, eval_mode, cfg)
+
+        if rank == 0:
+            for (cls_idx, iou) in enumerate(iou_class):
+                logger.info('***** Evaluation ***** >>>> Class [{:} {:}] '
+                            'IoU: {:.2f}'.format(cls_idx, CLASSES[cfg['dataset']][cls_idx], iou))
+            logger.info('***** Evaluation {} ***** >>>> MeanIoU: {:.2f}\n'.format(eval_mode, mIoU))
+            
+            writer.add_scalar('eval/mIoU', mIoU, epoch)
+            for i, iou in enumerate(iou_class):
+                writer.add_scalar('eval/%s_IoU' % (CLASSES[cfg['dataset']][i]), iou, epoch)
+
+        is_best = mIoU > previous_best
+        previous_best = max(mIoU, previous_best)
+        if rank == 0:
+            checkpoint = {
+                'model': model.state_dict(),
+                'optimizer': optimizer.state_dict(),
+                'epoch': epoch,
+                'previous_best': previous_best,
+            }
+            torch.save(checkpoint, os.path.join(args.save_path, 'latest.pth'))
+            if is_best:
+                torch.save(checkpoint, os.path.join(args.save_path, 'best.pth'))
+
+
+if __name__ == '__main__':
+    main()
